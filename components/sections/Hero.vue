e01567321857be248b12f973590d27100e85ddf5
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2021-01-19 11:23:07 +0100

    refactor hero section

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="top-content">
     <div class="hero">
-      <p class="hero__intro">Hi! my name is</p>
+      <p class="hero__intro">Hi! 👋🏿 my name is</p>
       <h1 class="hero__name">Iniubong Obonguko.</h1>
       <h2 class="hero__title">Web developer extraordinaire.</h2>
       <p class="hero__bio">
@@ -10,85 +10,7 @@
       </p>
       <a href="mailto:iniubongobonguko2018@gmail.com" class="hero__btn">Let's talk</a>
     </div>
-    <div class="social_links">
-      <ul>
-        <li class="social_link">
-          <a href="https://github.com/codiini"
-            ><svg
-              class="github-icon"
-              width="23"
-              height="22"
-              viewBox="0 0 23 22"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15.3519 20.1667V16.6192C15.3878 16.1821 15.3262 15.7426 15.1712 15.3301C15.0163 14.9176 14.7715 14.5415 14.4533 14.2267C17.4551 13.9058 20.61 12.815 20.61 7.80998C20.6098 6.53016 20.0963 5.29942 19.176 4.37248C19.6118 3.25278 19.581 2.01514 19.0899 0.91665C19.0899 0.91665 17.9618 0.595817 15.3519 2.27332C13.1607 1.7039 10.851 1.7039 8.65978 2.27332C6.04985 0.595817 4.92175 0.91665 4.92175 0.91665C4.43071 2.01514 4.3999 3.25278 4.83571 4.37248C3.90849 5.30629 3.39453 6.54816 3.40168 7.83748C3.40168 12.8058 6.55654 13.8967 9.55844 14.2542C9.2439 14.5658 9.00133 14.9374 8.8465 15.3449C8.69167 15.7524 8.62805 16.1865 8.65978 16.6192V20.1667M8.65978 17.4167C3.87969 18.7917 3.87969 15.125 1.96765 14.6667L8.65978 17.4167Z"
-                stroke-width="1.63889"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </a>
-        </li>
-        <li class="social_link">
-          <a href="https://linkedin.com"
-            ><svg
-              class="linkedin-icon"
-              width="24"
-              height="22"
-              viewBox="0 0 24 22"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15.8241 7.33325C17.3454 7.33325 18.8044 7.91271 19.8802 8.94416C20.9559 9.97561 21.5602 11.3746 21.5602 12.8333V19.2499H17.7362V12.8333C17.7362 12.347 17.5347 11.8807 17.1761 11.5369C16.8176 11.1931 16.3312 10.9999 15.8241 10.9999C15.317 10.9999 14.8307 11.1931 14.4721 11.5369C14.1135 11.8807 13.9121 12.347 13.9121 12.8333V19.2499H10.088V12.8333C10.088 11.3746 10.6924 9.97561 11.7681 8.94416C12.8438 7.91271 14.3028 7.33325 15.8241 7.33325V7.33325Z"
-                stroke-width="1.63889"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M6.26402 8.25H2.43994V19.25H6.26402V8.25Z"
-                stroke-width="1.63889"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M4.35198 5.49992C5.40797 5.49992 6.26402 4.67911 6.26402 3.66659C6.26402 2.65406 5.40797 1.83325 4.35198 1.83325C3.29599 1.83325 2.43994 2.65406 2.43994 3.66659C2.43994 4.67911 3.29599 5.49992 4.35198 5.49992Z"
-                stroke-width="1.63889"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </a>
-        </li>
-        <li class="social_link">
-          <a href="https://twitter.com/IObonguko"
-            ><svg
-              class="twitter-icon"
-              width="23"
-              height="22"
-              viewBox="0 0 23 22"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g clip-path="url(#clip0)">
-                <path
-                  d="M21.9885 2.74994C21.073 3.36913 20.0593 3.84271 18.9866 4.15244C18.4108 3.51766 17.6456 3.06774 16.7944 2.86354C15.9433 2.65934 15.0473 2.7107 14.2276 3.01069C13.4079 3.31068 12.704 3.84481 12.2112 4.54085C11.7185 5.23689 11.4605 6.06125 11.4723 6.90244V7.81911C9.79218 7.86088 8.12741 7.5036 6.62621 6.7791C5.12501 6.0546 3.83397 4.98536 2.86809 3.66661C2.86809 3.66661 -0.955982 11.9166 7.64818 15.5833C5.67929 16.8647 3.33382 17.5073 0.956055 17.4166C9.56022 21.9999 20.0764 17.4166 20.0764 6.87494C20.0755 6.61961 20.0499 6.3649 19.9999 6.11411C20.9757 5.19148 21.6642 4.02659 21.9885 2.74994V2.74994Z"
-                  stroke-width="1.63889"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0">
-                  <rect width="22.9444" height="22" fill="white" />
-                </clipPath>
-              </defs></svg
-          ></a>
-        </li>
-      </ul>
-    </div>
+    
   </section>
 </template>
 
@@ -99,12 +21,6 @@ export default {};
 <style lang="scss" scoped>
 .top-content {
   @include basic-flex;
-}
-.social_links ul {
-  flex-direction: column;
-}
-.social_links .social_link {
-  margin-top: 50px;
 }
 .hero {
   padding-top:100px;
@@ -142,6 +58,14 @@ export default {};
       color: #fff;
       transition: 0.3s ease-in-out;
     }
+  }
+}
+@media screen and (max-width:700px) {
+  .social_links{
+    display: none;
+  }
+  .hero__bio{
+    width:100%;
   }
 }
 </style>
